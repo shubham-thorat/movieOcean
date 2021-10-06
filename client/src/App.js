59958129ab84add99872de62038dd70b60be8742
Shubham Thorat <thoratshubham048@gmail.com>
2021-10-06 21:45:15 +0530

    Updated for mobile view

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import MyNavbar from './components/MyNavbar'
 import dotenv from 'dotenv'
 import './App.css'
 import { movieContext } from './helpers/Contexts'
+import { isMobile } from 'react-device-detect'
 import {
   Container
 } from 'react-bootstrap'
@@ -13,17 +14,28 @@ dotenv.config();
 function App() {
   const [movies, setmovies] = useState([])
   const [Dmovies, setDmovies] = useState([])
-
+  const [show, setshow] = useState(true)
   const fetchData = async () => {
     const response = await fetch('/movies');
     const data = await response.json();
     setmovies(data);
     setDmovies(data);
   }
+  const handleclick = () => {
+    setshow(false)
+  }
   useEffect(() => {
     fetchData();
   }, [])
 
+  if (isMobile && show) {
+    return (
+      <div>
+        <h4> Recommended to continue to desktop </h4>
+        <button onClick={handleclick} style={{ cursor: "pointer" }}> Continue </button>
+      </div>
+    )
+  }
 
   return (
     <div className="App">
@@ -36,7 +48,9 @@ function App() {
         </Container>
       </movieContext.Provider>
     </div>
+
   );
+
 }
 
 export default App;
